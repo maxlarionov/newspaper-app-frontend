71f082cf7243a3d091b883e8b1ec6b3c063f2a45
maxlarionov <maksym.larionov@gmail.com>
2023-08-28 10:52:46 +0300

    styled-component added

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,50 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { store } from './app/store'
+import reportWebVitals from './reportWebVitals'
+import './index.css'
+import { Paths } from './paths'
 
-const container = document.getElementById('root')!;
-const root = createRoot(container);
+const router = createBrowserRouter([
+  {
+    path: Paths.home,
+    element: <h1>Main</h1>
+  },
+  {
+    path: Paths.login,
+    element: <h1>Login</h1>
+  },
+  // {
+  //   path: Paths.register,
+  //   element: <Register />
+  // },
+  // {
+  //   path: Paths.articleAdd,
+  //   element: <AddArticle />
+  // },
+  // {
+  //   path: `${Paths.status}/:status`,
+  //   element: <Status />
+  // },
+  // {
+  //   path: `${Paths.employee}/:id`,
+  //   element: <Employee />
+  // },
+  // {
+  //   path: `${Paths.articleEdit}/:id`,
+  //   element: <EditArticle />
+  // },
+])
+
+const container = document.getElementById('root')!
+const root = createRoot(container)
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
@@ -20,4 +52,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
---
+++ src/paths.ts
@@ -0,0 +1,9 @@
+export const Paths = {
+	home: '/',
+	article: '/article',
+	articleAdd: '/article/add',
+	articleEdit: '/article/edit',
+	status: '/status',
+	login: '/login',
+	register: '/register',
+} as const
