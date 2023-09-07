96e5fa80fe4754298f22884bdbac89d53603c859
maxlarionov <maksym.larionov@gmail.com>
2023-09-07 15:04:03 +0300

    rtk query - auth

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,16 +1,20 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
+import auth from '../features/auth/authSlice'
+import { api } from './services/api'
 
 export const store = configureStore({
   reducer: {
+    [api.reducerPath]: api.reducer,
+    auth,
 
   },
-});
+})
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
   unknown,
   Action<string>
->;
+>
---
+++ src/types/user-types.ts
@@ -0,0 +1,21 @@
+export type User = {
+	id: string;
+	email: string;
+	password: string;
+	name: string;
+	accessEditing: boolean;
+}
+
+export type Article = {
+	id: string;
+	title: string;
+	text: string;
+	picture: string;
+	time: string;
+	userId: string;
+}
+
+export type Tag = {
+	id: string;
+	name: string;
+}
